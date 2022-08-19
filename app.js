c2c63207b84b6344b60661886e7f3e1826626df0
gejadzeomar <104693200+gejadzeomar@users.noreply.github.com>
2022-08-19 16:17:06 +0400

    1

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+const myName = "Omar" ,
+    myHobby = "codding" ,
+    yearOfBirth = 1989 ,
+    currentYear = 2022 ;
+
+    let aboutMe = "My name is " 
+    + myName +  " I am " +
+     (currentYear - yearOfBirth) + 
+     " years old and my hobby is " + 
+     myHobby; 
+    
+// console.log(aboutMe);
+myAge = (currentYear - yearOfBirth);
+let user = {
+    name: "Omar",
+    yearOfBirth: 1989,
+    age: myAge,
+    adress: "tbilisi",
+    email: "example@gmail.com",
+    tel: {
+        home: 12345678,
+        mob: 598123456
+    },
+
+}
+
+
+// console.log(user);
+
+
+let myArr = [2, 5, 9, 7, 4];
+let myArrLength = myArr.length;
+let myArrSum = myArr[0] + myArr[1] + myArr[2] + myArr[3] + myArr[4];
+let myArrAvverage = myArrSum / myArrLength;
+// console.log(myArrAvverage);
+
+let person = {
+    firstname: "Nino",
+    lastName: "Arveladze",
+    age: 20,
+    phoneNumbers: [123456, 654321],
+    // phone: {
+    //     work: phoneNumbers[0],
+    //     home: phoneNumbers[1],
+    // }
+}
+console.log(person);
+
+
+
