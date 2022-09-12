9b251832ef488f6092940f1300d6f226ec5cebb6
gejadzeomar <104693200+gejadzeomar@users.noreply.github.com>
2022-09-12 16:39:48 +0400

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ let myArr = [2, 5, 9, 7, 4];
 let myArrLength = myArr.length;
 let myArrSum = myArr[0] + myArr[1] + myArr[2] + myArr[3] + myArr[4];
 let myArrAvverage = myArrSum / myArrLength;
-// console.log(myArrAvverage);
+// console.log('averrage is ' + myArrAvverage);
 
 let person = {
     firstname: "Nino",
@@ -57,8 +57,35 @@ let person = {
 // let personObjNino = myObjArr[0];
 // console.log(personObjNino);
 
-console.log(
-    "my name is" {person.firstname}, "my age is ${person.age}, my adress is ${person.adress[0]}"
-    );
+// console.log("my name is" + person.firstname, "my age is " + person.age, 'my adress is ' + person.adress[0]);
 
-console.log(person.firstname)
+// console.log(person.firstname)
+
+let totalPrice = 200;
+let distance = 15;
+let deliveryPrice = 10;
+
+// if(distance < 10){
+//     console.log("delivery price is 20$")
+// }
+
+// console.log('example text');
+
+// if(distance < 10){
+//     console.log("delivery price is 20$")
+// } else {
+//     console.log("delivery price is 100$")
+// }
+
+// console.log('example text');
+
+if(distance < 10) {
+    console.log("delivery price is 20$")
+} else if(distance >= 10 < 20) {
+    console.log("delivery price is 30$")
+} else if(distance >= 20 && distance < 50) {
+    console.log("delivery price is 40$")
+} else {
+    console.log("delivery price is 0$")
+}
+console.log('example text');
