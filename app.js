cb07710dc96ec89b08d710d8045f0a5372970e8c
gejadzeomar <104693200+gejadzeomar@users.noreply.github.com>
2022-09-16 19:25:02 +0400

    Update app.js

--- app.js
+++ app.js
@@ -79,13 +79,74 @@ let deliveryPrice = 10;
 
 // console.log('example text');
 
-if(distance < 10) {
-    console.log("delivery price is 20$")
-} else if(distance >= 10 < 20) {
-    console.log("delivery price is 30$")
-} else if(distance >= 20 && distance < 50) {
-    console.log("delivery price is 40$")
-} else {
-    console.log("delivery price is 0$")
+// if(distance < 10) {
+//     console.log("delivery price is 20$")
+// } else if(distance >= 10 < 20) {
+//     console.log("delivery price is 30$")
+// } else if(distance >= 20 && distance < 50) {
+//     console.log("delivery price is 40$")
+// } else {
+//     console.log("delivery price is 0$")
+// }
+// console.log('example text');
+
+// if (distance < 10 || totalPrice >= 200) {
+//     console.log('delivery price is 20$');
+// }
+
+// switch (distance) {
+//     case 10:
+//         console.log('delivery price is 10$');
+//         break;
+
+//     case 20:
+//         console.log('delivery price is 20$');
+//         break;
+
+//     default:
+//         console.log('delivery price is 20$');
+//         break;
+
+// }
+
+// console.log('example text');
+
+// for (let i = 0; i < 10; i++) {
+//     // console.log("text", i)
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     console.log("text", i);
+//     i++
+// }
+
+// let myArrFor = [2, 4, 6, 8, 10];
+// let myArrForLength = myArrFor.length;
+// console.log(myArrFor);
+// let sumFor = 0;
+
+// for (let index = 0; index < myArrForLength; index++) {
+//     sumFor = sumFor + index;
+//     console.log(sumFor, myArrFor[index]);
+   
+// }
+
+// console.log(sumFor / myArrForLength);
+
+// let j = 10;
+// do {
+//     console.log('do while', j);
+//     j++;
+// } while (j < 5);
+
+let myArrFor = [2, 4, 6, 8, 10];
+let sumFor = 0;
+for (let index = 0; index < myArrFor.length; index++) {
+    sumFor = sumFor + myArrFor[index];
+    averrage = sumFor / myArrFor.length;
+    console.log(sumFor);
+    console.log("average is ", averrage);
 }
-console.log('example text');
+
+
